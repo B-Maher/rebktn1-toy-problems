dda78ecc724d9cc17dbce0d83fd05a958d97b8aa
B.Maher <bouzid.meher@gmail.com>
2019-12-28 09:19:25 +0100

    evenOccurrence done

--- evenOccurrence/evenOccurrence.js
+++ evenOccurrence/evenOccurrence.js
@@ -7,3 +7,22 @@ evenOccurrence([ "cat", "dog", "dig", "cat" ]) // "cat"
 evenOccurrence([[1,2,3], [1,2], [1,2,3]]) // [1,2,3]
 evenOccurrence([{a: 1, b: 2}, {a:1, b:3}, {a:1, b:2}]) // {a:1, b:2}
 */
+
+function evenOccurrence(array) {
+    let ocurrances = new Map();
+    for (let i = 0; i < array.length; i++) {
+        let elm = JSON.stringify(array[i]);
+        let value = ocurrances.get(elm);
+        if (value) {
+            ocurrances.set(elm, value + 1);
+        } else {
+            ocurrances.set(elm, 1);
+        }
+    }
+    for (let [key, value] of ocurrances) {
+        if (value % 2 === 0) {
+            return JSON.parse(key);
+        }
+    }
+    return null;
+}
