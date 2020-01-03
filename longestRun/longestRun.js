3abe1ee40d447c8f3a1327b128b3348616921e89
B.Maher <bouzid.meher@gmail.com>
2020-01-03 09:14:18 +0100

    longest run done

--- longestRun/longestRun.js
+++ longestRun/longestRun.js
@@ -12,4 +12,29 @@ longestRun("abcdefgh") // [ 0, 0 ]
 longestRun("abccccccc") // [ 2, 8 ]
 */
 
-function longestRun(string) {}
+function longestRun(string, result, i, current, start, count) {
+    result = result || [0, 0];
+    i = i || 0; //to keep track of the current index
+    current = current || ""; // to keep track of the current char
+    start = start || 0;
+    count = count || 0;
+
+    //check if the current char is the same as the previous
+    if (string[0] === current) {
+        count += 1; // increment the count
+    } else {
+        //check if the current count is bigger than the last combination
+        if ((result[1] - result[0]) < count) {
+            result = [start, start + count]; //update the result
+        }
+        //change the current lettre and reset the count and the starting value
+        current = string[0];
+        start = i;
+        count = 0;
+    }
+    if (string.length === 0) {
+        return result
+    }
+    //redo the same things with recursion
+    return longestRun(string.slice(1), result, i + 1, current, start, count)
+}
